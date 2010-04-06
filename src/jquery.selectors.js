e07e4f50d75cd9f76f0a3e5aacca7b889b24c79f
Jared Grippe <jared@deadlyicon.com>
2010-04-06 14:26:16 -0700

    added beginings of jquery.selectors.js and tests

--- src/jquery.selectors.js
+++ src/jquery.selectors.js
@@ -3,4 +3,53 @@
   if (!jQuery)   throw new Error('jquery.selectors.js requires jQuery 1.4.2 or later');
   if (!Selector) throw new Error('jquery.selectors.js requires Selectors 0.1 or later');
 
+  var DOCUMENT = $(document);
+
+  $.fn.extend({
+    toSelector: function toSelector(){
+      return new Selector(this.selector);
+    },
+    to: function to(query){
+      return this.toSelector().to(query);
+    },
+    from: function from(query){
+      return this.toSelector().from(query);
+    },
+    up: function up(query, plus){
+      return this.find(this.from(query)+(plus||""));
+    },
+    down: function down(query, plus){
+      // console.log('down', this, arguments, this.toSelector());
+      var parent_selector = this.toSelector(),
+          selector = parent_selector.down(query);
+      if (plus) selector = selector.plus(plus);
+
+      var collection = this.find(selector.from(parent_selector));
+      collection.toSelector = function toSelector(){ return selector; };
+      console.assert( collection.selector === selector.toString() );
+      return collection;
+    }
+    // plus ??
+  });
+
+  $.extend(Selector.prototype, {
+    get: function(){
+      var selector = this, collection = $(this.toString());
+      collection.toSelector = function toSelector(){ return selector; };
+      console.assert( collection.selector === selector.toString() );
+      return collection;
+    },
+    bind: function(types, data, handler){
+      var event_name, selector = this.fullValue();
+
+      if (typeof types === 'string')
+        DOCUMENT.delegate(selector, types, data, handler);
+      else
+        for (event_name in types)
+          DOCUMENT.delegate(selector, event_name, types[event_name]);
+
+      return this;
+    }
+  });
+
 })(jQuery, Selector);
--- test/unit/jquery.selectors_test.js
+++ test/unit/jquery.selectors_test.js
@@ -1,10 +1,83 @@
 ;(function() {
-  var undefined;
+
+  var undefined, events = $(document).delegate('a','a',function(){}).data().events.live = [];
 
   module("Selector");
 
+  $('html body').append($(
+    '<div class="content" style="display:none">'+
+      '<div class="profile">'+
+        '<div class="content">'+
+          '<img src=""/>'+
+        '</div>'+
+      '</div>'+
+    '</div>'
+  ));
+
+  S('body')
+    .def('content', '>.')
+      .def('profile', '>.')
+        .def('content', '>.')
+          .def('image', '> img').end
+        .end
+      .end
+    .end
+  .end;
+
+  test('selector.get', function(){
+    var body_selector   = S('body'),
+        body_collection = $('body');
+
+    expect( body_selector.get().toSelector()       ).toBeTheSameSelectorAs(body_selector);
+    expect( body_selector.get()                    ).toReferenceTheSameHtmlElementsAs(body_collection);
+    expect( body_selector.plus('> .content').get() ).toReferenceTheSameHtmlElementsAs(body_collection.find('> .content'));
+  });
+
   test("selector.bind", function() {
     expect(Selector().bind).toBeAnInstanceOf(Function);
+
+    var handler = function(){}, data = {};
+
+    expect(events.length).toEqual(0);
+
+    Selector('a').bind('test', data, handler);
+    expect(events[0].selector).toEqual('a');
+    expect(events[0].origType).toEqual('test');
+    expect(events[0].data    ).toBe(data);
+    expect(events[0].handler ).toBe(handler);
+
+    Selector('a').bind('test', handler);
+    expect(events[1].selector).toEqual('a');
+    expect(events[1].origType).toEqual('test');
+    expect(events[1].handler ).toBe(handler);
+
+    Selector('span').bind({test: handler});
+    expect(events[2].selector).toEqual('span');
+    expect(events[2].origType).toEqual('test');
+    expect(events[2].handler ).toBe(handler);
+
+    events.length = 0;
   });
+
+  module("jQuery");
+
+  test('jQuery().toSelector', function(){
+    expect( $('html body').toSelector().toString()  ).toBe('html body');
+    expect( S('body').get().toSelector()            ).toBeTheSameSelectorAs(S('body'));
+    expect( S('body').get().toSelector().up('html') ).toBeTheSameSelectorAs(S('html'));
+  });
+
+  test('jQuery().to, jQuery().to', function(){
+
+  });
+
+  test('jQuery().up, jQuery().down', function(){
+    var html = S('html').get();
+    expect( html.down('content')           ).toReferenceTheSameHtmlElementsAs( $('html body > .content')                       );
+    expect( html.down('profile')           ).toReferenceTheSameHtmlElementsAs( $('html body > .content > .profile')            );
+    expect( html.down('profile content')   ).toReferenceTheSameHtmlElementsAs( $('html body > .content > .profile > .content') );
+    expect( html.down('profile').down('content')   ).toReferenceTheSameHtmlElementsAs( $('html body > .content > .profile > .content') );
+  });
+
 
 })();
